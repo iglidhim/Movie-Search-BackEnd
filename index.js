3b4413595c5dbacbe0ed7d930c914220d064ad56
Igli Dhima <iglidhim@amazon.com>
2021-06-28 10:05:25 -0400

    make it better

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 require('dotenv').config()
+var mongoSrivice = require('httpmongodb');
 const Fetch = require('node-fetch');
 const Bluebird = require('bluebird');
- 
 Fetch.Promise = Bluebird;
+
+
 
 const port = process.env.PORT;
 
@@ -13,12 +15,19 @@ app.get('/', (req, res) => {
   res.send('My Movie Search API!')
 })
 
+app.use(mongoSrivice("USA/NewYork", true));
+app.use(express.json());
+
 app.use(cors())
 app.get('/search/:keyword', (req, res, next)=>{
     Fetch(`http://www.omdbapi.com/?s=${req.params.keyword}&apikey=5cb03411`)
     .then(res => res.json())
     .then(json => res.send({status: true, Search: json.Search}));
 })
+
+
+app.use('/favourites', require("./favourites"))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
